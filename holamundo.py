f6cd603a36be578e26aaadc930d63a835c8a1549
Alberto Quintana <91602621+Albertill0@users.noreply.github.com>
2024-04-16 12:20:54 +0200

    Update holamundo.py

--- holamundo.py
+++ holamundo.py
@@ -1,1 +1,21 @@
-print("Hola Mundo")
+import pickle
+
+def hola_mundo():
+    print("Hola mundo!")
+
+# Ejemplo de clase vulnerable que utiliza pickle
+class MiClase:
+    def __reduce__(self):
+        return (eval, ("print('¡Hola desde la clase!')",))
+
+# Crear una instancia de la clase
+objeto_vulnerable = MiClase()
+
+# Serializar el objeto utilizando pickle
+datos_serializados = pickle.dumps(objeto_vulnerable)
+
+# Deserializar los datos (potencialmente peligroso)
+objeto_deserializado = pickle.loads(datos_serializados)
+
+# Llamar al método __reduce__ que ejecuta código arbitrario
+objeto_deserializado.__reduce__()
